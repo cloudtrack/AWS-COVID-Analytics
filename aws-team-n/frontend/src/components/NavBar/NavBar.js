282e5b7ff8856952698e8d8b95951e33a2ee0b32
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-11-28 09:01:29 +0900

    add code for NavBar

--- aws-team-n/frontend/src/components/NavBar/NavBar.js
+++ aws-team-n/frontend/src/components/NavBar/NavBar.js
@@ -6,10 +6,10 @@ const NavBar = () => {
     return(
         <div className="NavBar">
             <div className="Tabs">
-                <li className={path == "/main" && "active"}><a href="/main">COVID</a></li>
-                <li className={path == "/air-pollution" && "active"}><a href="/air-pollution">Air</a></li>
-                <li className={path == "/stock-market" && "active"}><a href="/stock-market">Stock</a></li>
-                <li className={path == "/unemployment" && "active"}><a href="/unemployment">Unemployment</a></li>
+                <li className={path === "/main" && "active"}><a href="/main">COVID</a></li>
+                <li className={path === "/air-pollution" && "active"}><a href="/air-pollution">Air</a></li>
+                <li className={path === "/stock-market" && "active"}><a href="/stock-market">Stock</a></li>
+                <li className={path === "/unemployment" && "active"}><a href="/unemployment">Unemployment</a></li>
             </div>
             Hello
         </div>
