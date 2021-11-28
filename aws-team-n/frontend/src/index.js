cedf470bd1857698e2aade8cf1e490c40c83b5e8
나혜주 <hyejuna@nahyejuui-MacBook-Pro-2.local>
2021-11-28 22:56:54 +0900

    setup store

--- aws-team-n/frontend/src/index.js
+++ aws-team-n/frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import store, { history } from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
