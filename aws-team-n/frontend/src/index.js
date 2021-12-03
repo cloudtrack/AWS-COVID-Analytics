c24673383c41b93f55c38517a5d411f9ebcbb215
나혜주 <hyejuna@nahyejuui-MacBook-Pro-2.local>
2021-12-03 21:28:16 +0900

    revert index.js changes as store is removed

--- aws-team-n/frontend/src/index.js
+++ aws-team-n/frontend/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store, { history } from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App history={history} />
-  </Provider>,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
