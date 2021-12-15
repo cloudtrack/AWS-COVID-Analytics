138d9eb2d37c9426cfdf09a2cab9ee36738e90c1
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-12-16 00:23:53 +0900

    add code for predictions - no initial data

--- aws-team-n/frontend/src/containers/AirPollution/Graph2/graph2.js
+++ aws-team-n/frontend/src/containers/AirPollution/Graph2/graph2.js
@@ -1,63 +1,147 @@
 import React, {Component} from "react";
-import * as d3 from "d3";
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+import no2_covid_p10 from "../data/aws-forecasts-no2_covid_p10_df.json"
+import no2_covid_p50 from "../data/aws-forecasts-no2_covid_p50_df.json"
+import no2_covid_p90 from "../data/aws-forecasts-no2_covid_p90_df.json"
+import no2_p10 from "../data/aws-forecasts-no2_p10_df.json"
+import no2_p50 from "../data/aws-forecasts-no2_p50_df.json"
+import no2_p90 from "../data/aws-forecasts-no2_p90_df.json"
+import o3_covid_p10 from "../data/aws-forecasts-o3_covid_p10_df.json"
+import o3_covid_p50 from "../data/aws-forecasts-o3_covid_p50_df.json"
+import o3_covid_p90 from "../data/aws-forecasts-o3_covid_p90_df.json"
+import o3_p10 from "../data/aws-forecasts-o3_p10_df.json"
+import o3_p50 from "../data/aws-forecasts-o3_p50_df.json"
+import o3_p90 from "../data/aws-forecasts-o3_p90_df.json"
+import so2_covid_p10 from "../data/aws-forecasts-so2_covid_p10_df.json"
+import so2_covid_p50 from "../data/aws-forecasts-so2_covid_p50_df.json"
+import so2_covid_p90 from "../data/aws-forecasts-so2_covid_p90_df.json"
+import so2_p10 from "../data/aws-forecasts-so2_p10_df.json"
+import so2_p50 from "../data/aws-forecasts-so2_p50_df.json"
+import so2_p90 from "../data/aws-forecasts-so2_p90_df.json"
+
+
+const list = {
+    'NO2': [no2_covid_p10, no2_covid_p50, no2_covid_p90, no2_p10, no2_p50, no2_p90],
+    'SO2': [so2_covid_p10, so2_covid_p50, so2_covid_p90, so2_p10, so2_p50, so2_p90],
+    'O3': [o3_covid_p10, o3_covid_p50, o3_covid_p90, o3_p10, o3_p50, o3_p90]
+}
 class Graph2 extends Component{
     state = {
-        data: null
+        data: {
+            'NO2': null,
+            'SO2': null,
+            'O3': null
+        }
     }
     componentDidMount(){
-        var data = null
-        d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', function(rows){
-            function unpack(rows, key) {
-                return rows[key]
+        const getFileName = (i, name) => {
+            var type = ""
+            if ((i % 3) == 0){
+                type = "p10 "
+            } else if((i % 3) == 1){
+                type = "p50 "
+            } else {
+                type = "p90 "
             }
-            console.log(rows)
-            data = [{
-                type: 'choropleth',
-                locations: unpack(rows, 'CODE'),
-                z: unpack(rows, 'GDP (BILLIONS)'),
-                text: unpack(rows, 'COUNTRY'),
-                colorscale: [
-                    [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-                    [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-                    [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
-                autocolorscale: false,
-                reversescale: true,
-                marker: {
-                    line: {
-                        color: 'rgb(180,180,180)',
-                        width: 0.5
-                    }
-                },
-                tick0: 0,
-                zmin: 0,
-                dtick: 1000,
-                colorbar: {
-                    autotic: false,
-                    tickprefix: '$',
-                    title: 'GDP<br>Billions US$'
-                }
-            }];
+            name += type
+            if (i < 3){
+                name += "(w/ covid)"
+            }
+            return name
+        }
+        const datasets = ['NO2', 'SO2', 'O3'].map((idx) => {
+            return list[idx].map((file, i) => {
+                const values = file.map(data => {
+                    return Number(data['Value'])
+                })
+                const ret = {}
+                ret['label'] = getFileName(i, idx)
+                ret['data'] = values
+                ret['borderColor'] = i < 3 ? `rgba(255, 150, 132, ${(i+2)/4})` : `rgba(132, 99, 255, ${1/5 + (i%3+1)/7})`
+                ret['backgroundColor'] =  i < 3 ? `rgba(255, 150, 132, ${(i+2)/4})` : `rgba(132, 99, 255, ${1/5 + (i%3+1)/7})`
+                return ret
+            })
         })
+        const labels = no2_covid_p10.map(data => {
+            return data['Timestamp'].split('T')[0]
+        })
+        const data = {
+            'NO2': {
+                labels,
+                datasets: datasets[0]
+            },
+            'SO2': {
+                labels,
+                datasets: datasets[1]
+            },
+            'O3': {
+                labels,
+                datasets: datasets[2]
+            },
+          };
+
         this.setState({data: data})
     }
 
-    layout = {
-        title: '2014 Global GDP<br>Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html"> CIA World Factbook</a>',
-        geo:{
-            showframe: false,
-            showcoastlines: false,
-            projection:{
-                type: 'mercator'
-            }
-        }
-    };
-
     render(){
+        const getOptions = (idx) => {
+            const options = {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: {
+                    position: 'top',
+                    labels: {
+                        font:{
+                            size: 14
+                        }
+                    },
+                  },
+                  title: {
+                    display: true,
+                    text: idx,
+                    font: {
+                        size: 25
+                    }
+                  },
+                },
+            };
+            return options
+        }
+
 
         return (
             <div>
-
+                <div className="so2-chart" style={{height: "600px", marginBottom: "50px"}}>
+                    {this.state.data['SO2'] && <Line options={getOptions('SO2')} data={this.state.data['SO2']} />}
+                </div>
+                <div className="o3-chart" style={{height: "600px"}}>
+                    {this.state.data['O3'] && <Line options={getOptions('O3')} data={this.state.data['O3']} />}
+                </div>
+                <div className="no2-chart" style={{height: "600px"}}>
+                    {this.state.data['NO2'] && <Line options={getOptions('NO2')} data={this.state.data['NO2']} />}
+                </div>
             </div>
         )
     }
