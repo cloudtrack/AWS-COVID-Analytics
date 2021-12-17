f024c0ea6816870ce628708ef481b308f73655e8
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-12-17 09:20:23 +0900

    graph1

--- aws-team-n/frontend/src/containers/AirPollution/Graph1/graph1.js
+++ aws-team-n/frontend/src/containers/AirPollution/Graph1/graph1.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import { GeoJSON, MapContainer, TileLayer, Marker, Tooltip, Popup } from "react-leaflet";
+import React, {Component, useMemo} from "react";
+import { GeoJSON, MapContainer, TileLayer, Marker, Tooltip, Popup, useMap } from "react-leaflet";
 import * as L from "leaflet";
 
 import {Slider} from '@mui/material';
@@ -11,6 +11,60 @@ import o3_2020_12_28 from '../data/o3-2020-12-28.json'
 
 const MAX = 365
 
+// Classes used by Leaflet to position controls
+const POSITION_CLASSES = {
+    bottomleft: 'leaflet-bottom leaflet-left',
+    bottomright: 'leaflet-bottom leaflet-right',
+    topleft: 'leaflet-top leaflet-left',
+    topright: 'leaflet-top leaflet-right',
+  }
+const getColor = (d) => {
+    return d > 50 ? '#800026' :
+           d > 30  ? '#BD0026' :
+           d > 25  ? '#E31A1C' :
+           d > 15  ? '#FC4E2A' :
+           d > 5   ? '#FD8D3C' :
+           d > 2   ? '#FEB24C' :
+           d > 1   ? '#FED976' :
+                      '#AAAAAA';
+}
+const Legend = ({position, zoom}) => {
+    const positionClass =
+        (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright
+
+    const grades = [0, 10, 20, 50, 100, 200, 500, 1000]
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    const Grades = grades.map((g, i) => {
+        return <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <div style={{"backgroundColor": getColor(g + 1), width: '18px', height: '18px', float: 'left', marginRight: '8px', opacity: 0.7}}></div>
+                    <div style={{lineHeight: '18px', color: '#555'}}>
+                        {g}
+                        {grades[i+1] ? <span> &ndash; {grades[i+1]}</span> : '+'}
+                    </div>
+                </div>
+    })
+    return (
+        <div className={positionClass}>
+            <div style={{marginBottom: 10, marginRight: 10, padding: 8, backgroundColor: '#FFFFFFCC', borderRadius: 5,
+            boxShadow: "5px 5px 5px #AAAAAA"}}>
+                {Grades}
+            </div>
+            <div style={{paddingBottom: 17}}></div>
+        </div>
+    )
+}
+
+const Info = ({position, _}) => {
+    const positionClass =
+        (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright
+    return (
+        <div className={positionClass}>
+            Hello
+        </div>
+    )
+
+}
 class Graph1 extends Component{
     state = {
         // mapSource: o3_2020_12_28,
@@ -33,7 +87,7 @@ class Graph1 extends Component{
     componentDidMount(){
         const date = "2021-04-29"
         var features = source['features'].filter((val) => {
-            return val['properties']['date'] == date
+            return val['properties']['date'] == date || val['properties']['date'] == null
         })
         const data = {
             "type": "FeatureCollection",
@@ -41,22 +95,13 @@ class Graph1 extends Component{
         }
         this.setState({data: data})
 
-        console.log(this.refs.map);
+        // console.log(this.refs.map);
     }
 
-    getColor = (d) => {
-        return d > 50 ? '#800026' :
-               d > 30  ? '#BD0026' :
-               d > 25  ? '#E31A1C' :
-               d > 15  ? '#FC4E2A' :
-               d > 5   ? '#FD8D3C' :
-               d > 2   ? '#FEB24C' :
-               d > 1   ? '#FED976' :
-                          '#AAAAAA';
-    }
+
     style = (feature) => {
         return {
-            fillColor: this.getColor(feature.properties.avg),
+            fillColor: getColor(feature.properties.avg),
             weight: 2,
             opacity: 1,
             color: 'white',
@@ -75,7 +120,7 @@ class Graph1 extends Component{
         this.setState({prevVal: newVal})
         console.log(this.state.shortDate)
         var features = source['features'].filter((val) => {
-            return val['properties']['date'] == date
+            return val['properties']['date'] == date || val['properties']['date'] == null
         })
         const data = {
             "type": "FeatureCollection",
@@ -85,26 +130,27 @@ class Graph1 extends Component{
         this.setState({loading: false})
     }
 
+
     render(){
         const outerBounds = [
                 [84.227542, -168.776165],
                 [-84.999685, 188.872239],
               ]
 
-
         return (
-            <div>
-                <h1 style={{ textAlign: "center" }}>My Map</h1>
+            <div style={{justifyContent: "center", marginLeft: 50, marginRight: 50, marginBottom: 50
+                }}>
                 {
                     this.state.loading ? <h1>LOADING</h1>
                     :
                     <div style={{alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-                        <div style={{width: "80%", borderWidth: 1, margin: 50, backgroundColor: 'red'}}>
+                        <div style={{width: "90%", borderWidth: 1, marginTop: 10, marginBottom: 10,}}>
                             <Slider defaultValue={0} step={1} min={0} max={MAX}
                             valueLabelDisplay="off" onChange={this.handleSliderChange}
                             />
+                            <h3 >{this.state.shortDate}</h3>
                         </div>
-                        <h3>{this.state.shortDate}</h3>
+
                         <MapContainer style={{ height: "80vh", padding: 50, backgroundColor: '#F8F8F8' }} zoom={1.5}
                             center={[20.955188, 11.566430]} maxZoom={5} minZoom={0} bounds={outerBounds} maxBounds={outerBounds}
                         >
@@ -123,11 +169,29 @@ class Graph1 extends Component{
                                 onEachFeature={(feature, layer) => {
                                     layer.on({
                                         'mouseover': (e) => {
-                                          layer.bindTooltip(feature.properties.avg.toString());
-                                        }
-                                      });
+                                            if (feature.properties.avg != null){
+                                                layer.bindTooltip(feature.properties.avg.toString());
+                                            }
+                                            // layer.setStyle({
+                                            //     weight: 5,
+                                            //     color: '#666',
+                                            //     dashArray: '',
+                                            //     fillOpacity: 0.7
+                                            // });
+                                            // layer.bringToFront();
+                                        },
+                                        // 'mouseout': (e) => {
+                                        //     var geojson;
+                                        //     // ... our listeners
+                                        //     geojson = L.geoJson();
+                                        //     geojson.resetStyle(e.target);
+                                        // }
+                                    });
+
                                 }}
                             />
+                            <Info position={"topright"}/>
+                            <Legend position={"bottomright"}/>
                         </MapContainer>
                     </div>
                 }
